8f4430e726c6965c515d1719f3e5aa3edaa742a2
CDees <bcd021@latech.edu>
2026-03-31 19:04:19 -0500

    Add coroutine to tumbling

--- 2026-Game477-3D-main/Assets/Scripts/Tumbler.cs
+++ 2026-Game477-3D-main/Assets/Scripts/Tumbler.cs
@@ -1,36 +1,42 @@
 using System;
+using System.Collections; 
 using System.Collections.Generic;
 using UnityEngine;
 using State = TumblerState;
 
-public enum TumblerState {
-  IDLE,
-  TUMBLING_UP,
-  TUMBLING_DOWN,
-  DISABLED
+public enum TumblerState
+{
+    IDLE,
+    TUMBLING_UP,
+    TUMBLING_DOWN,
+    DISABLED
 }
 
-public class Tumbler : MonoBehaviour {
-  public int numberOfSides;
+public class Tumbler : MonoBehaviour
+{
+    public int numberOfSides;
 
-  public State State { get; private set; }
-  private HashSet<KeyValuePair<State, State>> allowedTransitions;
+    public State State { get; private set; }
+    private HashSet<KeyValuePair<State, State>> allowedTransitions;
 
-  private Dictionary<State, Action> stateEnterMethods;
-  private Dictionary<State, Action> stateStayMethods;
-  private Dictionary<State, Action> stateExitMethods;
+    private Dictionary<State, Action> stateEnterMethods;
+    private Dictionary<State, Action> stateStayMethods;
+    private Dictionary<State, Action> stateExitMethods;
 
-  private int number;
-  private Quaternion curRot;
-  private Quaternion targetRot;
-  private float t;
+    private int number;
+    private Quaternion curRot;
+    private Quaternion targetRot;
+    private float t;
 
-  private void Start() {
-    State = State.IDLE;
+    private Coroutine tumblingCouroutine; 
 
-    number = 0;
+    private void Start()
+    {
+        State = State.IDLE;
 
-    allowedTransitions = new() {
+        number = 0;
+
+        allowedTransitions = new() {
       new(State.IDLE, State.TUMBLING_UP),
       new(State.IDLE, State.TUMBLING_DOWN),
       new(State.TUMBLING_UP, State.IDLE),
@@ -38,101 +44,140 @@ public class Tumbler : MonoBehaviour {
       new(State.IDLE, State.DISABLED),
     };
 
-    stateEnterMethods = new() {
-      [State.IDLE] = StateEnter_Idle,
-      [State.TUMBLING_UP] = StateEnter_TumblingUp,
-      [State.TUMBLING_DOWN] = StateEnter_TumblingDown,
-      [State.DISABLED] = StateEnter_Disabled,
-    };
-    stateStayMethods = new() {
-      [State.IDLE] = StateStay_Idle,
-      [State.TUMBLING_UP] = StateStay_TumblingUp,
-      [State.TUMBLING_DOWN] = StateStay_TumblingDown,
-      [State.DISABLED] = StateStay_Disabled,
-    };
-    stateExitMethods = new() {
-      [State.IDLE] = StateExit_Idle,
-      [State.TUMBLING_UP] = StateExit_TumblingUp,
-      [State.TUMBLING_DOWN] = StateExit_TumblingDown,
-      [State.DISABLED] = StateExit_Disabled,
-    };
-  }
-
-  private void Update() {
-    if (stateStayMethods.ContainsKey(State)) {
-      stateStayMethods[State].Invoke();
+        stateEnterMethods = new()
+        {
+            [State.IDLE] = StateEnter_Idle,
+            [State.TUMBLING_UP] = StateEnter_TumblingUp,
+            [State.TUMBLING_DOWN] = StateEnter_TumblingDown,
+            [State.DISABLED] = StateEnter_Disabled,
+        };
+        stateStayMethods = new()
+        {
+            [State.IDLE] = StateStay_Idle,
+            [State.TUMBLING_UP] = StateStay_TumblingUp,
+            [State.TUMBLING_DOWN] = StateStay_TumblingDown,
+            [State.DISABLED] = StateStay_Disabled,
+        };
+        stateExitMethods = new()
+        {
+            [State.IDLE] = StateExit_Idle,
+            [State.TUMBLING_UP] = StateExit_TumblingUp,
+            [State.TUMBLING_DOWN] = StateExit_TumblingDown,
+            [State.DISABLED] = StateExit_Disabled,
+        };
     }
-  }
 
-  //public int GetNumber() {
-  //  return number + 1;
-  //}
-  public int GetNumber() => number + 1;
 
-  public void ChangeState(State newState) {
-    if (allowedTransitions.Contains(new(State, newState))) {
-      stateExitMethods[State].Invoke();
-      State = newState;
-      stateEnterMethods[State].Invoke();
+    private void Update()
+    {
+        if (stateStayMethods.ContainsKey(State))
+        {
+            stateStayMethods[State].Invoke();
+        }
+        Tumble(); 
     }
-  }
 
-  private void StateEnter_Idle() {
-    // idle enter method
-  }
-  private void StateEnter_TumblingUp() {
-    curRot = transform.rotation;
-    targetRot = curRot * Quaternion.Euler(Vector3.up * (360 / numberOfSides));
-    t = 0;
-  }
-  private void StateEnter_TumblingDown() {
-    curRot = transform.rotation;
-    targetRot = curRot * Quaternion.Euler(Vector3.down * (360 / numberOfSides));
-    t = 0;
-  }
-  private void StateEnter_Disabled() {
-    // used enter method
-  }
 
-  private void StateStay_Idle() {
-    // idle enter method
-  }
-  private void StateStay_TumblingUp() {
-    t += 1 * Time.deltaTime;
-    transform.rotation = Quaternion.Lerp(curRot, targetRot, t);
-    if (t >= 1) {
-      transform.rotation = targetRot;
-      ChangeState(State.IDLE);
+    private IEnumerator Tumble()
+    {
+        while(t < 1)
+        {
+            t += 1 * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(curRot, targetRot, t); 
+            yield return new WaitForEndOfFrame(); 
+        }
+        transform.rotation = targetRot;
+        ChangeState(State.IDLE); 
     }
-  }
-  private void StateStay_TumblingDown() {
-    t += 1 * Time.deltaTime;
-    transform.rotation = Quaternion.Lerp(curRot, targetRot, t);
-    if (t >= 1) {
-      transform.rotation = targetRot;
-      ChangeState(State.IDLE);
-    }
-  }
-  private void StateStay_Disabled() {
-    // used enter method
-  }
 
-  private void StateExit_Idle() {
-    // idle enter method
-  }
-  private void StateExit_TumblingUp() {
-    number++;
-    number %= numberOfSides;
-    print($"number from tumbler is: {GetNumber()} from {name}");
-  }
-  private void StateExit_TumblingDown() {
-    number--;
-    if (number < 0) {
-      number += numberOfSides;
+
+    //public int GetNumber() {
+    //  return number + 1;
+    //}
+
+
+    public int GetNumber() => number + 1;
+
+    public void ChangeState(State newState)
+    {
+        if (allowedTransitions.Contains(new(State, newState)))
+        {
+            stateExitMethods[State].Invoke();
+            State = newState;
+            stateEnterMethods[State].Invoke();
+        }
     }
-    print($"number from tumbler is: {GetNumber()} from {name}");
-  }
-  private void StateExit_Disabled() {
-    // used enter method
-  }
+
+    private void StateEnter_Idle()
+    {
+        // idle enter method
+    }
+
+    private void StateEnter_TumblingUp()
+    {
+        curRot = transform.rotation;
+        targetRot = curRot * Quaternion.Euler(Vector3.up * (360 / numberOfSides));
+        t = 0;
+        tumblingCouroutine = StartCoroutine(Tumble()); 
+    }
+
+    private void StateEnter_TumblingDown()
+    {
+        curRot = transform.rotation;
+        targetRot = curRot * Quaternion.Euler(Vector3.down * (360 / numberOfSides));
+        t = 0;
+        StartCoroutine(Tumble());
+    }
+
+    private void StateEnter_Disabled()
+    {
+        // used enter method
+    }
+
+    private void StateStay_Idle()
+    {
+        // idle enter method
+    }
+
+    private void StateStay_TumblingUp()
+    {
+  
+    }
+
+    private void StateStay_TumblingDown()
+    {
+
+    }
+
+    private void StateStay_Disabled()
+    {
+        // used enter method
+    }
+
+    private void StateExit_Idle()
+    {
+        // idle enter method
+    }
+
+    private void StateExit_TumblingUp()
+    {
+        number++;
+        number %= numberOfSides;
+        print($"number from tumbler is: {GetNumber()} from {name}");
+    }
+
+    private void StateExit_TumblingDown()
+    {
+        number--;
+        if (number < 0)
+        {
+            number += numberOfSides;
+        }
+        print($"number from tumbler is: {GetNumber()} from {name}");
+    }
+
+    private void StateExit_Disabled()
+    {
+        // used enter method
+    }
 }
