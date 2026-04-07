f78a81f0658e50acd21397e1e53cac7ea7ae8e99
CDees <bcd021@latech.edu>
2026-04-06 21:04:33 -0500

    Add materials and progress game manager script

---
+++ 2026-Game477-3D-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using GameState = FsmGameState;
+
+public enum FsmGameState
+{
+    MainMenu,
+    Playing,
+    Paused,
+    GameOver,
+    Win,
+}
+
+// overall game manager done as a singleton 
+// handles game state and other managers 
+public class GameManager : MonoBehaviour
+{
+    public GameState GameState { get; private set; }
+
+    private void Start()
+    {
+        GameState = GameState.MainMenu;
+    }
+
+    private void Update()
+    {
+        switch (GameState)
+        {
+            case GameState.MainMenu:
+                // main menu logic 
+                break;
+
+            case GameState.Playing:
+                break; 
+        }
+    }
+}
