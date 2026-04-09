1c82ccd4d39e1c153bfce8d54ad4ffc12085e793
Mav-24 <1cwd05@gmail.com>
2026-04-09 15:52:14 -0500

    added book for my room and edited final item script for uniformity

--- 2026-Game477-3D-main/Assets/Scripts/CalebScripts/InventoryItem_Emerald.cs
+++ 2026-Game477-3D-main/Assets/Scripts/CalebScripts/InventoryItem_Emerald.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class InventoryItem_Emerald : InventoryItem
 {
+
+    public string puzzlename;
+
     public override void Use()
     {
         print("chaos control");
-        GameManager.Instance.MarkPuzzleComplete("CalebPuzzle");
+        GameManager.Instance.MarkPuzzleComplete(puzzlename);
     }
 }
