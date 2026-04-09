481dfc64bb771e76274840ada94f00aa9d9f8c30
CDees <bcd021@latech.edu>
2026-04-09 01:48:45 -0500

    Establish puzzle FSM

--- 2026-Game477-3D-main/Assets/Scripts/ChandlerPuzzleScripts/PuzzleController.cs
+++ 2026-Game477-3D-main/Assets/Scripts/ChandlerPuzzleScripts/PuzzleController.cs
@@ -13,11 +13,11 @@ public class PuzzleController : MonoBehaviour
 {
     // singleton 
     public static PuzzleController Instance { get; private set; }
-
+    public PuzzleState PuzzleState { get; private set; }
     // a list of book objects
     public Books[] slots;
     // thing we spawn that is used to complete the game 
-    public GameObject winObject; 
+    public GameObject winObject;
 
 
     private void Awake()
@@ -25,40 +25,69 @@ public class PuzzleController : MonoBehaviour
         Instance = this;
     }
 
-    private void Start()
-    {
-
-    }
-
-    private void Update()
-    {
-
-    }
-
     public void CheckSolved()
     {
+        int solvedCount = 0;
+
         foreach (var slot in slots)
         {
             if (!slot.isFilled)
-                return; 
+                solvedCount++;
         }
-    }
 
-    private void Solve()
-    {
-        winObject.SetActive(true);
-        print("Chandler puzzle win obj spawned");
+        // handles the actual puzzle state swapping
+        switch (solvedCount)
+        {
+            case 0:
+                // really shouldn't come up, should be default
+                // added just in case 
+                ChangeState(PuzzleState.NoProgress);
+                break;
+
+            case 1:
+                ChangeState(PuzzleState.OneShelfSolved);
+                break;
+
+            case 2:
+                ChangeState(PuzzleState.TwoShelvesSolved);
+                break;
+
+            case 3:
+                ChangeState(PuzzleState.PuzzleComplete);
+                break;
+        }
     }
 
     public void ChangeState(PuzzleState newState)
     {
-        // no redundant swaps 
-        //if (PuzzleState == newState) return;
+        //no redundant swaps
+        if (PuzzleState == newState) return;
 
-        //switch (PuzzleState)
-        //{
+        PuzzleState = newState;
 
-        //}
+        switch (PuzzleState)
+        {
+            case PuzzleState.NoProgress:
+                print("No shelves solved");
+                // keep all light objects turned off
+                break;
+
+            case PuzzleState.OneShelfSolved:
+                print("One shelf solved");
+                // turn on one light object
+                break;
+
+            case PuzzleState.TwoShelvesSolved:
+                print("Two shelves solved");
+                // turn on two light objects
+                break;
+
+            case PuzzleState.PuzzleComplete:
+                print("All shelves solved");
+                // turn on all three light objects
+                winObject.SetActive(true);
+                break;
+        }
     }
 
 
